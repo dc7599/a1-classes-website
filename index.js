cfc49285f7e6dbe9723ad9fd83a9edb48c80ef52
dc7599 <dc7599chaudhary@gmail.com>
2025-04-06 17:26:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ export default function Home() {
   return (
     <div style={{ fontFamily: 'Arial', textAlign: 'center', padding: '50px' }}>
       <h1>Welcome to A1 Classes</h1>
+      <p>Empowering Education. Shaping Futures.</p>
+    </div>
+  );
+}export default function Home() {
+  return (
+    <div style={{ fontFamily: 'Arial', textAlign: 'center', padding: '50px' }}>
+      <h1>Welcome to A1 Classes</h1>
       <p>Empowering Education, Shaping Futures</p>
     </div>
   );
